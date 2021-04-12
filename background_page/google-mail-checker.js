da83fd2e8bd5d74ce4412065ba7331c90a2ab97a
Michael <mml.liu@mail.utoronto.ca>
2021-04-12 01:26:26 -0400

    Add ES6 modules and ESLint for HTML

---
+++ background_page/google-mail-checker.js
@@ -0,0 +1,36 @@
+import {
+  getOpenGmailTabsInCurrentWindow,
+  isCurrentTabInCurrentWindowGmail,
+  openNewGmailTab,
+  switchToFirstTabAndActivate
+} from './tab/tab.js';
+
+const onClickIcon = async () => {
+  // check if Gmail tab exists already
+  let openGmailTabs = [];
+  try {
+    console.log(await getOpenGmailTabsInCurrentWindow());
+    openGmailTabs = [...openGmailTabs, ...(await getOpenGmailTabsInCurrentWindow()).flat()];
+  } catch (err) {
+    console.error(`An error occurred: ${err}`);
+    return;
+  }
+
+  if (openGmailTabs.length !== 0) {
+    // if current tab is already Gmail, do nothing
+    if (await isCurrentTabInCurrentWindowGmail()) {
+      return;
+    }
+    // switch focus to the Gmail tab
+    try {
+      await switchToFirstTabAndActivate(openGmailTabs);
+    } catch (err) {
+      console.error(`An error occurred: ${err}`);
+      return;
+    }
+  } else {
+    await openNewGmailTab();
+  }
+};
+
+browser.browserAction.onClicked.addListener(onClickIcon);
---
+++ background_page/tab/tab.js
@@ -0,0 +1,51 @@
+export const getOpenGmailTabsInCurrentWindow = () => browser.tabs.query(
+  {
+    currentWindow: true,
+    url: '*://mail.google.com/*'
+  }
+);
+
+/**
+ * Sets the focus to the first Gmail tab in the current window
+ * @param {tabs.Tab[]} openGmailTabs
+ * @returns {Promise<tabs.Tab[]>} the tab that was opened
+ */
+export const switchToFirstTabAndActivate = (openGmailTabs) => {
+  const [firstOpenGmailTab] = openGmailTabs;
+  return browser.tabs.update(
+    firstOpenGmailTab.id,
+    {
+      active: true
+    }
+  );
+};
+
+export const openNewGmailTab = async () => {
+  try {
+    await browser.tabs.create(
+      {
+        active: true,
+        url: 'https://gmail.com'
+      }
+    );
+  } catch (err) {
+    console.error(`An error occurred: ${err}`);
+    return;
+  }
+};
+
+export const isCurrentTabInCurrentWindowGmail = async () => {
+  // precondition: >= 1 Gmail tabs are open
+  try {
+    const [currentTab] = await browser.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      }
+    );
+    return currentTab.active && currentTab.url.includes('mail.google.com');
+  } catch (err) {
+    console.error(`An error occurred: ${err}`);
+    return;
+  }
+};
